ca2c3ba36c12e91d93ae42ab5f6b24faca15781d
Bob <bob@bob.com>
2020-06-27 10:33:30 -0400

    Ajout de fonction.h et modifs de main.cpp

---
+++ fonction.h
@@ -0,0 +1,10 @@
+#ifndef __FONCTIONS_H__
+#define __FONCTIONS_H__
+
+#include <iostream>
+
+void afficher(int valeur) {
+    std::cout << valeur << std::endl;
+}
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
+#include "fonction.h"
+
 int main() {
 	std::cout << "TEST !" << std::endl;
+    int x = 50;
+    x = x + 1;
+    afficher(x);
 }
