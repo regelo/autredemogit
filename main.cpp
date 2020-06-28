7153698381486ea2b130b120a77d3b0cd51e0b9f
karim107-git <karim107@yahoo.fr>
2020-06-27 22:35:38 -0400

    Ajout d'une nouvelle fonction random number

--- main.cpp
+++ main.cpp
@@ -1,25 +1,17 @@
 ﻿#include <iostream>
 
 #include "fonction.h"
+#include "fonction2.h"
 
 int main() {
-
-
-std::cout << " Voila le premier transfere de Git Anoymous  !" << std::endl;
-
-
-    int x = 145788850;
-
-    x = x + 1;
-
-    x = 5;
-
-    afficher(x);
-    
-    std::cout << "nouvel essai" << std::endl;
-
-
- 
+std::cout << " Voila le premier transfere de Git   !" << std::endl;
+int x = 145788850;
+x = x + 1;
+x = 5;
+afficher(x);
+double y=Generer(x);
+std ::cout<<"Generation d'un nombre aleatoire : "<<y<<std::endl;    
+std::cout << "nouvel essai" << std::endl; 
 }
 
 
